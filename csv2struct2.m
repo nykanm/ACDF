57f367109a4a7d0f897f5a8cb7b3620b313c3b67
nykanm <nyk.mirchi@gmail.com>
2019-01-18 16:43:31 -0500

    fixed csv_concat and csv2struct2

---
+++ csv2struct2.m
@@ -0,0 +1,49 @@
+function [RawData] = csv2struct2(loadfolder,DataDir)
+% Function converts csv files into a structure. 
+% csv2struct2(loadfolder) where loadfolder is '1M_Compact', '2J_Compact',
+% '3S_Compact', or '4C_Compact'
+loaddir = [DataDir loadfolder];
+
+subdir = dir(loaddir);
+
+if strcmp(subdir(3).name,'.DS_Store')==1
+    n=4;
+else
+    n=3;
+end
+
+for i=n:length(subdir) 
+    filepath = [subdir(i).folder '/' subdir(i).name];
+
+    ins = readtable(filepath);
+
+    Instr = table2struct(ins,'ToScalar',true);
+
+    fields = fieldnames(Instr);
+
+    for idx=1:length(fieldnames(Instr))
+        if n==3
+            RawData(i-2).(fields{idx}) = vertcat(Instr.(fields{idx}));
+        elseif n==4
+            RawData(i-3).(fields{idx}) = vertcat(Instr.(fields{idx}));
+        end
+    end
+end
+old = cd;
+cd '/Users/nykan/Documents/McGill Grad/Matlab/ACDF/Sample Data/';
+
+if strcmp(loadfolder,'1M_Compact')==1
+    RawData_1M = RawData;
+    save(['Data_' loadfolder '.mat'],'RawData_1M')
+elseif strcmp(loadfolder,'2J_Compact')==1
+    RawData_2J = RawData;
+    save(['Data_' loadfolder '.mat'],'RawData_2J')
+elseif strcmp(loadfolder,'3S_Compact')==1
+    RawData_3S = RawData;
+    save(['Data_' loadfolder '.mat'],'RawData_3S')
+elseif strcmp(loadfolder,'4C_Compact')==1
+    RawData_4C = RawData;
+    save(['Data_' loadfolder '.mat'],'RawData_4C')
+end
+
+cd(old);
--- csv_concat.m
+++ csv_concat.m
@@ -1,26 +1,49 @@
+function [] = csv_concat(loadfolder,DataDir)
+% This function concatenates all your participants' toollogs into one
+% toollog for each participant saved in CompactTools folder. To use, type
+% csv_concat(loadfolder) where loadfolder is '1M' or '2J' or '3R' or '4C'
+
 % Use this file to concatenate all your csv files from the OSSim into one
 % big csv file for all tools used.
 
 % Then type "csv2struct" in your command line and load the file "tools.csv"
-
 % Change this to the directory where your ToolData files are:
-loaddir = '/Users/nykan/Documents/McGill Grad/Matlab/ACDF/Data/ACDF85/ACDF_20181212105246/ToolLog/';
-% Change this to the directory where you want to save tools.csv
-savedir = '/Users/nykan/Documents/McGill Grad/Matlab/ACDF/Data/ACDF85/ACDF_20181212105246/ToolLog/';
+loaddir = [DataDir loadfolder];
+savedir = ['/Users/nykan/Documents/McGill Grad/Matlab/ACDF/Sample Data/'];
 
+
+%% csv_concat
 subdir = dir(loaddir);
 
 for i=3:length(subdir)
-    filename = [loaddir '/' subdir(i).name];
-    csv{i-2} = readtable(filename);
+    disp(i-2);
+    foldername = [loaddir '/' subdir(i).name];
+    subdir2 = dir(foldername);
+    if length(subdir2)==3
+        foldername2 = [foldername '/' subdir2(3).name '/ToolLog'];
+    elseif length(subdir2)==4
+        foldername2 = [foldername '/' subdir2(4).name '/ToolLog'];
+    end
+        subdir3 = dir(foldername2);
+    for ii=3:length(subdir3)
+        disp(ii-2);
+        filename = [foldername2 '/' subdir3(ii).name];
+        csv{ii-2} = readtable(filename);
+    end
+        allcsv = vertcat(csv{1},csv{2});
+        for o=3:length(csv)
+            allcsv = vertcat(allcsv,csv{o});
+        end
+
+        allcsv = sortrows(allcsv,'TimeSinceStart');
+        
+        writetable(allcsv,[savedir loadfolder '_Compact/' subdir(i).name '.csv']);
+        clear csv;
+        clear allcsv;
+    
 end
 
-allcsv = vertcat(csv{1},csv{2});
-for o=3:length(csv)
-    allcsv = vertcat(allcsv,csv{o});
-end
 
-allcsv = sortrows(allcsv,'TimeSinceStart');
 
-writetable(allcsv,[savedir 'tools.csv']);
+
 
